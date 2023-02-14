04a8a69c34609146fb171b7a522fb2ae4e36f973
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-02-14 23:29:35 +0300

    feat: init project
    - init `tailwind-css`
    - init `headless-ui`

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="p-9">
+	<h1 class="text-3xl font-bold">Hello world!</h1>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		min-height: 100vh;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		background-color: theme(colors.gray.100);
+	}
+</style>
